eddaf7ebbcb7821751196e4fc73dd6b2f7524d0f
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-08-15 17:51:17 +0300

    init: create task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.mid = Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
